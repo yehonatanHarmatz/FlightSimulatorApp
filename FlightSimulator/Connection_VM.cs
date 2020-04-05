3a8b5d5e847ec5550496be13740a6c6578b613fe
yehonatanHarmatz <47951976+yehonatanHarmatz@users.noreply.github.com>
2020-04-05 18:40:51 +0300

    Add files via upload

---
+++ FlightSimulator/Connection_VM.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightSimulatorApp
+{
+    class Connection_VM
+    {
+        private Model m;
+
+
+        public void Connect()
+        {
+            try
+            {
+                this.m.Start();
+            } catch (Exception e)
+            {
+                /***TO-DO***/
+            }
+        }
+        public void Disonnect()
+        {
+            try
+            {
+                this.m.Stop();
+            }
+            catch (Exception e)
+            {
+                /***TO-DO***/
+            }
+        }
+    }
+}
