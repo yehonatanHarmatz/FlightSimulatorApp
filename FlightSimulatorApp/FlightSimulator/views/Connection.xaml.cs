a01a902349ffec0030c36c4da43913b1f62d5d94
yehonatanHarmatz <47951976+yehonatanHarmatz@users.noreply.github.com>
2020-04-12 17:42:43 +0300

    Add files via upload

---
+++ FlightSimulatorApp/FlightSimulator/views/Connection.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FlightSimulatorApp.views
+{
+    /// <summary>
+    /// Interaction logic for Connection.xaml
+    /// </summary>
+    public partial class Connection : UserControl, INotifyPropertyChanged
+    {
+        private bool isConnect = false;
+        private Connection_VM cvm;
+        private string text = "The Server is: Disconnect";
+        
+        public Connection()
+        {
+            InitializeComponent();
+        }
+        
+        public Connection(Connection_VM cvm)
+        {
+            InitializeComponent();
+            this.cvm = cvm;
+            DataContext = this;
+        }
+
+        public string Text
+        {
+            get => text; 
+            set
+            {
+                if (!text.Equals(value))
+                {
+                    text = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Text"));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void ClickConnect(object sender, EventArgs e)
+        {
+            if (cvm != null)
+            {
+                cvm.Connect();
+                isConnect = true;
+                Text = "The Server is: Connect";
+            }
+        }
+        private void Connect(object sender, RoutedEventArgs e)
+        {
+            if (!isConnect)
+            {
+                ConnectionWindow cw = new ConnectionWindow();
+                cw.listeners += ClickConnect;
+                cw.Show();
+               
+            }
+            else
+            {
+                /*******error message******/
+            }
+
+        }
+        private void Disonnect(object sender, RoutedEventArgs e)
+        {
+            if (isConnect)
+            {
+                if (cvm != null)
+                {
+                    cvm.Disonnect();
+                    isConnect = false;
+                    Text = "The Server is: Disconnect";
+                }
+            }
+        }
+    }
+}
---
+++ FlightSimulatorApp/FlightSimulator/views/Map.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FlightSimulatorApp.views
+{
+    /// <summary>
+    /// Interaction logic for Map.xaml
+    /// </summary>
+    public partial class Map : UserControl
+    {
+        Map_VM map;
+        public Map()
+        {
+            InitializeComponent();
+        }
+        public Map(Map_VM map)
+        {
+            InitializeComponent();
+            this.map = map;
+            DataContext = map;
+            //Model m = new Model();
+            //Map_VM map = new Map_VM(m);
+            //DataContext = map;
+            //Thread t1 = new Thread(m.Start);
+            //t1.Start();
+        }
+    }
+}
